f830c5f422f1958e62f8f2a38268cd99fc9f9c38
Domara Shlimon <domara2000@gmail.com>
2021-06-05 00:49:27 -0400

    added end of the game

---
+++ Assets/Scripts/EventsAndActivators/DisableTimerOnEvent.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class DisableTimerOnEvent : ActivateOnEvent {
+
+	protected override void OnActivate() {
+		Schedule.PauseTimer = true;
+	}
+
+}
